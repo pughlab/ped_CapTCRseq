0eb8d19caacb45688d44f2ed0feafc44d74c4614
Arash Nabbi <nabbi.arash@gmail.com>
2022-11-18 14:52:54 -0500

    sample heatmap based on spearman correlation among counts of significant GLIPH clusters

--- R/color_schemes.R
+++ R/color_schemes.R
@@ -2,33 +2,38 @@ group_col <- c("Leukemia" = "#1B9E77",
                "Lymphoma" = "#D95F02",
                "Solid" = "#7570B3")
 
-cohort_col <- c("ALL" = "", #leukemia
-                "AML" = "",
-                "BL" = "",
-                "CML" = "",
-                "HR ALL" = "",
-                "SR ALL" = "",
-                "T-ALL" = "",
-                "ALCL" = "", # Lymphoma
-                "BL" = "",
-                "BLL" = "",
-                "DLBCL" = "",
-                "HD" = "",
-                "PMBCL" = "",
+
+cohort_col <- c("ALL" = "#eff3ff", #leukemia
+                "AML" = "#c6dbef",
+                "BL" = "#9ecae1",
+                "CML" = "#6baed6",
+                "HR ALL" = "#4292c6",
+                "SR ALL" = "#2171b5",
+                "T-ALL" = "#084594",
+                "ALCL" = "#feedde", # Lymphoma
+                "BL" = "#fdd0a2",
+                "BLL" = "#fdae6b",
+                "DLBCL" = "#fd8d3c",
+                "HD" = "#f16913",
+                "PMBCL" = "#d94801",
                 "ARMS" = "#76afa9", #Solid
                 "ERMS" = "#a6cee3", 
                 "EWS" =  "#1f78b4",
+                "RMS" = "#153f5c",
+                "Normal" = "#bec2be", 
+                "LFS" = "black",
+                "MS NB" = "#33a02c",
                 "OS" =  "#678ba5",
                 "NB" =  "#33a02c",
                 "HB" = "#e31a1c",
                 "WILMS" = "#fdbf6f")
 
 
-cycle_col <- c("01" = "#1B9E77",
-               "02" = "#D95F02",
-               "03" = "#D95F02",
-               "04" = "#D95F02",
-               "05" = "#D95F02")
+cycle_col <- c("01" = "#1f78b4",
+               "02" = "#a6cee3",
+               "03" = "#b2df8a",
+               "04" = "#33a02c",
+               "05" = "#e31a1c")
 
 age_pal <- c("Infants" = "#bd0026",
             "Children" = "#fc4e2a",
@@ -36,7 +41,6 @@ age_pal <- c("Infants" = "#bd0026",
             "Young adults" = "#ffeda0",
             "Adults" = "#80cdc1",
             "Unknown" = "#878787")
-
 
 
 
