b7e0d3e94d10b698a6ff725e798a53209ece8fe3
Arash Nabbi <nabbi.arash@gmail.com>
2022-11-21 16:57:07 -0500

    update color scheme

--- R/color_schemes.R
+++ R/color_schemes.R
@@ -1,39 +1,40 @@
 group_col <- c("Leukemia" = "#1B9E77",
                "Lymphoma" = "#D95F02",
-               "Solid" = "#7570B3")
-
+               "Solid" = "#7570B3",
+               "LFS" = "black",
+               "Normal" ="light grey")
 
 cohort_col <- c("ALL" = "#eff3ff", #leukemia
                 "AML" = "#c6dbef",
-                "BL" = "#9ecae1",
                 "CML" = "#6baed6",
                 "HR ALL" = "#4292c6",
                 "SR ALL" = "#2171b5",
                 "T-ALL" = "#084594",
-                "ALCL" = "#feedde", # Lymphoma
-                "BL" = "#fdd0a2",
-                "BLL" = "#fdae6b",
-                "DLBCL" = "#fd8d3c",
-                "HD" = "#f16913",
-                "PMBCL" = "#d94801",
-                "ARMS" = "#76afa9", #Solid
-                "ERMS" = "#a6cee3", 
-                "EWS" =  "#1f78b4",
-                "RMS" = "#153f5c",
+                "ALCL" = "#bcbddc", # Lymphoma
+                "BL" = "#9e9ac8",
+                "BLL" = "#807dba",
+                "DLBCL" = "#6a51a3",
+                "HD" = "#4a1486",
+                "PMBCL" = "#4a1486",
+                "ARMS" = "#fc9272", #Solid
+                "ERMS" = "#fb6a4a", 
+                "EWS" =  "#ef3b2c",
+                "RMS" = "#cb181d",
+                "OS" =  "#99000d",
                 "Normal" = "#bec2be", 
                 "LFS" = "black",
-                "MS NB" = "#33a02c",
-                "OS" =  "#678ba5",
-                "NB" =  "#33a02c",
-                "HB" = "#e31a1c",
-                "WILMS" = "#fdbf6f")
+                "MS NB" = "#a6761d",
+                "NB" =  "#a6761d",
+                "HB" = "#01665e",
+                "WILMS" = "#e9a3c9")
 
 
-cycle_col <- c("01" = "#1f78b4",
-               "02" = "#a6cee3",
-               "03" = "#b2df8a",
-               "04" = "#33a02c",
-               "05" = "#e31a1c")
+cycle_col <- c("X00" = "black",
+  "X01" = "#1f78b4",
+               "X02" = "#a6cee3",
+               "X03" = "#b2df8a",
+               "X04" = "#33a02c",
+               "X05" = "#e31a1c")
 
 age_pal <- c("Infants" = "#bd0026",
             "Children" = "#fc4e2a",
@@ -42,6 +43,7 @@ age_pal <- c("Infants" = "#bd0026",
             "Adults" = "#80cdc1",
             "Unknown" = "#878787")
 
-
+type_col <- structure(c("#e41a1c", "#377eb8", "#4daf4a"),
+                      names = c("cfDNA", "PBMC", "Tumor"))
 
 
