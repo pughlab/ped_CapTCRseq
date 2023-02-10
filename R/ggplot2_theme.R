265e8c250da181e781a54c842d43e3fecabc029d
Arash Nabbi <nabbi.arash@gmail.com>
2023-02-10 16:25:10 -0500

    fix ggplot theme

--- R/ggplot2_theme.R
+++ R/ggplot2_theme.R
@@ -2,10 +2,10 @@ library(ggplot2)
 
 myaxis <- 
   #axis
-  theme(axis.title = element_text(size = 25),
+  theme(axis.title = element_text(size = 15),
         axis.line = element_line(color = "black"),
-        axis.text.x = element_text(size = 25, angle = 45, hjust = 1, color = "black"),
-        axis.text.y = element_text(size = 25, color = "black")) 
+        axis.text.x = element_text(size = 15, angle = 45, hjust = 1, color = "black"),
+        axis.text.y = element_text(size = 15, color = "black")) 
 myplot <-
   #plot
   theme(panel.grid.major = element_blank(),
@@ -15,9 +15,9 @@ myplot <-
   #legend
   theme(legend.key = element_rect(fill = "white", colour = "white"),
         legend.position = "right", 
-        legend.text = element_text( color = "black", size = 25),
+        legend.text = element_text( color = "black", size = 15),
         legend.title = element_blank(),
-        legend.key.size = unit(1, 'cm'))  +
-  theme(plot.title = element_text(size = 25, hjust = 0.5, colour = "black"))
+        legend.key.size = unit(0.5, 'cm'))  +
+  theme(plot.title = element_text(size = 15, hjust = 0.5, colour = "black"))
 
 
